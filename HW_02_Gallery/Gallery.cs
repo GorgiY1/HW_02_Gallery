b9af4ad21c83d15da5ecefac6e32fa42659f7aae
johnny <orgeorgij@gmail.com>
2024-12-29 19:21:14 +0200

    Add project files.

---
+++ HW_02_Gallery/Gallery.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace HW_02_Gallery
+{
+    [Serializable]
+    public class Images
+    {
+        public string Path { get; set; }
+        public string Mark { get; set; }
+
+        //[NonSerialized]
+        //public Image image_coll;
+    }
+
+    //[Serializable]
+    //public class Images2
+    //{
+    //    public string Path { get; set; }
+    //    public string Mark { get; set; }
+   // }
+    //class Gallery
+    //{
+    //    public static Dictionary<Image, int> _images;
+    //    public Gallery()
+    //    {
+    //        _images = new Dictionary<Image, int>();
+    //        //_images.Add("/bdd20b7d5b34486da3db84ce0c808b7a.jpg", 0);
+    //        //_images.Add("/1660828639184470996.png", 0);
+    //        //_images.Add("/166082864116043105.png", 0);
+    //        //_images.Add("/1660828646123530957.png", 0);
+    //        //_images.Add("/2f62db1ee120c81573d8813ac369952808f222aa_1024.jpg", 0);
+    //        //_images.Add("/39f2bade-723c57083305190737254934fe2a4064.jpeg", 0);
+    //        //_images.Add("/field_image_000foto_planeta_2016.jpg", 0);
+    //        //_images.Add("/images (1).jpg", 0);
+    //        //_images.Add("/images (2).jpg", 0);
+    //        //_images.Add("/images (3).jpg", 0);
+    //        //_images.Add("/images (4).jpg", 0);
+    //        //_images.Add("/images (5).jpg", 0);
+    //        //_images.Add("/images.jpg", 0);
+    //        //_images.Add("/Без названия.jpg", 0);
+    //        //_images.Add("/mp-cover.jpg", 0);
+    //    }
+    //    public Gallery(int grade, Image path)
+    //    {
+    //        _images = new Dictionary<Image, int>();
+    //        _images.Add(path, grade);
+    //    }
+    //    public void Add(Image path)
+    //    {
+    //        _images.Add(path, 0);
+    //    }
+    //    public void Add(Image path, int grade)
+    //    {
+    //        _images.Add(path, grade);
+    //    }
+
+    //    //public void Save_To_File(string path)
+    //    //{
+    //    //    using (FileStream fs = new FileStream(path,FileMode.CreateNew,FileAccess.Write))
+    //    //    {
+    //    //        using (StreamWriter sw = new StreamWriter(path))
+    //    //        {
+    //    //            foreach (string item in _images.Keys)
+    //    //            {
+    //    //                sw.WriteLine($"{item}, {_images[item]}");
+    //    //            }
+    //    //        }
+    //    //    }
+    //    //}
+
+    //    //public void Load_From_File(string path)
+    //    //{
+    //    //    using (FileStream fs = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+    //    //    {
+    //    //        using (StreamWriter sw = new StreamWriter(path))
+    //    //        {
+    //    //            foreach (string item in _images.Keys)
+    //    //            {
+    //    //                sw.WriteLine($"{item}, {_images[item]}");
+    //    //            }
+    //    //        }
+    //    //    }
+    //    //}
+    //}
+}
---
+++ HW_02_Gallery/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Serialization;
+
+namespace HW_02_Gallery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Images> gallery = new List<Images>();
+        //static List<Images2> gallery2 = new List<Images2>();
+        static Images im_tmp = new Images();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Images>));
+            try
+            {
+                using (FileStream stream = File.OpenRead("Marks.xml"))
+                {
+                    gallery = formatter.Deserialize(stream) as List<Images>;
+                }
+            }
+            catch
+            {
+                for (int i = 0; i < stack_images.Children.Count; i++)
+                {
+                    if (stack_images.Children[i] is Image im)
+                    {
+                       gallery.Add(new Images { /*image_coll = im,*/ Path = im.Source.ToString(), Mark = "0" });
+                    }
+                }
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        { 
+            if (sender is Slider slider)
+            {
+                for (int j = 0; j < gallery.Count && j < stack_images.Children.Count; j++)
+                {
+                    if (stack_images.Children[j] is Image image && j == slider.Value)
+                    {
+                        stack_images.Children[j].Visibility = Visibility.Visible;
+
+                        for (int l = 0; l < radio_btns.Children.Count; l++)
+                        {
+                            if (radio_btns.Children[l] is RadioButton rd_btn)
+                            {
+                                rd_btn.IsChecked = false;
+                                if (rd_btn.Content is string rdb_content1 && gallery[j].Mark == rdb_content1)
+                                {
+                                    if (gallery[j].Mark == "0") 
+                                    {
+                                        rd_btn.IsChecked = false;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        rd_btn.IsChecked = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        
+
+                        //for (int l = 0; l < radio_btns.Children.Count; l++)
+                        //{
+                        //    bool ischecked2 = false;
+                        //    foreach (Images item in gallery)
+                        //    {
+                        //        if (radio_btns.Children[l] is RadioButton rd_btn && rd_btn.Content is string rdb_content && int.Parse(rdb_content)-1 == l)
+                        //        {
+                        //            if (gallery.IndexOf(item) == (int)slider.Value)
+                        //            {
+                        //                ((RadioButton)radio_btns.Children[l]).IsChecked = false;
+
+                        //                ischecked2 = true;
+                        //                break;
+                        //            }
+                        //        }
+                        //    }
+                            
+                        //    if (ischecked2)
+                        //    {
+                        //        break;
+                        //    }
+                        //}
+
+                        for (int i = 0; i < stack_images.Children.Count; i++)
+                        {
+                            if (i != j)
+                            {
+                                stack_images.Children[i].Visibility = Visibility.Collapsed;
+                            }
+                            if (i == j)
+                            {
+                                stack_images.Children[j].Visibility = Visibility.Visible;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            if (stack_images.Children.Count >= slider_imeges.Value - 1)
+            {
+                slider_imeges.Value--;
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if ( slider_imeges.Value + 1 <= slider_imeges.Maximum)
+            {
+                slider_imeges.Value++;
+            }
+        }
+
+        private void rdbtn_Chacked(object sender, RoutedEventArgs e)
+        {
+            for (int j = 0; j < gallery.Count; j++)
+            {
+                if (stack_images.Children[j] is Image image && j == slider_imeges.Value)
+                {
+                    for (int l = 0; l < radio_btns.Children.Count; l++)
+                    {
+                        if (radio_btns.Children[l] is RadioButton rd_btn)
+                        {
+                            if (rd_btn.IsChecked == true)
+                            {
+                                if (rd_btn.Content is string rdb_content1)
+                                {
+                                    gallery[j].Mark = rdb_content1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            try
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Images>));
+                using (FileStream stream = File.Create("Marks.xml"))
+                {
+                    formatter.Serialize(stream, gallery);
+                }
+                //MessageBox.Show("\nSerialize OK!\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void radio_btns_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
